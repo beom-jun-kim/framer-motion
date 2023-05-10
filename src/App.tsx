20822faa2a4d84801ecb0555c06d25bcf77152e4
kimbeomjun <gimb52043@gmail.com>
2023-05-10 16:34:46 +0900

    variants

--- src/App.tsx
+++ src/App.tsx
@@ -15,21 +15,16 @@ const Box = styled(motion.div)`
   border-radius: 15px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
+
+const myVars = {
+  start: {scale: 0},
+  end: {scale: 1, rotateZ: 360 , transition:{type: "spring"}}
+}
+
 function App() {
   return (
     <Wrapper>
-
-      <Box
-
-        /* initial : 초기 상태 설정 */
-        initial={{ scale: 0 }}
-
-        /* animate : 최종 상태 설정 */
-        animate={{ scale: 1, rotateZ: 360 }}
-
-        /* transition : 동착 형태 설정 */
-        transition={{ type: "spring", /* stiffness:10 */ /* damping:1 */ }}
-      />
+      <Box variants={myVars} initial="start" animate="end"/>
     </Wrapper>
   );
 }
