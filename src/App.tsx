347360f8af9fbfe887bb68ac5843a85fb2ffcf97
kimbeomjun <gimb52043@gmail.com>
2023-05-17 15:19:36 +0900

    project01

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,40 @@
 import styled from "styled-components";
-import { motion } from "framer-motion";
+import { motion, AnimatePresence, animate } from "framer-motion";
 import { useState } from "react";
 
 const Wrapper = styled(motion.div)`
   width: 100vw;
   height: 100vh;
   display: flex;
-  justify-content: space-around;
+  justify-content: center;
   align-items: center;
+`;
+
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  width: 50vw;
+  gap: 10px;
+  div:last-child,
+  div:first-child {
+    grid-column: span 2;
+  }
 `;
 
 const Box = styled(motion.div)`
-  width: 300px;
-  height: 300px;
+  height: 200px;
   background-color: rgba(255, 255, 255, 1);
   border-radius: 20px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
-const Circle = styled(motion.div)`
-  background-color: #00a5ff;
-  height: 100px;
-  width: 100px;
-  border-radius: 50px;
-  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+const Overlay = styled(motion.div)`
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
 function App() {
@@ -34,10 +42,23 @@ function App() {
   const toggleClick = () => setClicked((prev) => !prev);
   return (
     <Wrapper onClick={toggleClick}>
-      <Box />
-      <Box />
-      <Box />
-      <Box />
+      <Grid>
+        <Box layoutId="box1"/>
+        <Box />
+        <Box />
+        <Box />
+      </Grid>
+      <AnimatePresence>
+        {clicked ? (
+          <Overlay
+            initial={{ backgroundColor: "rgba(0,0,0,0)" }}
+            animate={{ backgroundColor: "rgba(0,0,0,0.6)" }}
+            exit={{ backgroundColor: "rgba(0,0,0,0)" }}
+          >
+            <Box layoutId="box1" style={{ width: 400, height: 200 }} />
+          </Overlay>
+        ) : null}
+      </AnimatePresence>
     </Wrapper>
   );
 }
